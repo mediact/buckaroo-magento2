100ab651f41a02ab86ed3fdc67f60dee6139789b
Robert Grundeken <robert.grundeken@tig.nl>
2019-08-05 17:25:10 +0200

    Version bump

--- Service/Software/Data.php
+++ Service/Software/Data.php
@@ -43,7 +43,7 @@ class Data
     const MODULE_CODE = 'TIG_Buckaroo';
 
     /** Version of Module */
-    const BUCKAROO_VERSION = '1.11.0-RC2';
+    const BUCKAROO_VERSION = '1.11.0';
 
     /** @var ProductMetadataInterface */
     private $productMetadata;
